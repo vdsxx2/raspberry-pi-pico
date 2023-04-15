5bc7110eed3f0dd5bc47bbe2c79c55e2ca836634
vdsxx2 <130604460+vdsxx2@users.noreply.github.com>
2023-04-14 21:33:35 -0400

    Update blink.py

--- led blinking/blink.py
+++ led blinking/blink.py
@@ -1,1 +1,12 @@
+from machine import Pin
+from time import sleep
 
+led = Pin(25, Pin.OUT)
+
+n = 0
+
+while True:
+    led.toggle()
+#     print("13 times {} is {}".format(n, 13*n))
+#     n = n + 1
+    sleep(.25)
